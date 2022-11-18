ee7dac8cc3160d188a3e190ccbbef8a2a33d8f5f
Andrew Biryukov <biryukov.andrej2000@ispras.ru>
2022-11-18 16:27:43 +0300

    hw 6

--- HW_6/SeeSaw.py
+++ HW_6/SeeSaw.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-from itertools import tee, zip_longest
+from itertools import tee, zip_longest, filterfalse
 
 def seesaw(seq):
 	it1, it2 = tee(seq, 2)
-	for i, j in enumerate(zip_longest(it1, it2, fillvalue='_')):
-		print(i, j)
-		if i % 2:
-			yield j[1]
-		else:
-			yield j[0]
+	for i in zip_longest(filter(lambda x: not x % 2, it1), filter(lambda x: x % 2, it2), fillvalue=None):
+		for j in i:
+			if j != None:
+				yield j
 		
-		
-print(*seesaw(i//3 for i in range(1, 27, 2)))
