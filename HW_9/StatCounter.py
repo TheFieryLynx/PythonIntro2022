989b0e479e7311163053b8b893d9bb1b35e87aa9
Andrew Biryukov <biryukov.andrej2000@ispras.ru>
2022-11-28 16:30:59 +0300

    hw 9

--- HW_9/StatCounter.py
+++ HW_9/StatCounter.py
@@ -17,14 +17,14 @@ def statcounter():
 	while in_proc:
 		in_proc = yield decorator(in_proc)
 
-#stat = statcounter()
-#stats = next(stat)
-#
-#@stat.send
-#def f1(a): return a+1
-#	
-#@stat.send
-#def f2(a, b): return f1(a)+f1(b)
-#	
-#print(f1(f2(2,3)+f2(5,6)))
-#print(*((f.__name__, c) for f, c in stats.items()))
+stat = statcounter()
+stats = next(stat)
+
+@stat.send
+def f1(a): return a+1
+	
+@stat.send
+def f2(a, b): return f1(a)+f1(b)
+	
+print(f1(f2(2,3)+f2(5,6)))
+print(*((f.__name__, c) for f, c in stats.items()))
